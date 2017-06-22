49ac9cadcbfea4dc12922cf8669bcd264125bc44
Hoonie Sun <myeonghoon18@gmail.com>
2017-06-22 10:57:37 +1000

    loading Images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
+import './style.css';
+import Eggie from './eggie.jpg'
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
